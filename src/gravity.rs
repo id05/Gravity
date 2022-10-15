a78e74393f497a5bd066598f4612e5ce70c719bf
Vyacheslav Vetreny <451th.try@gmail.com>
2022-10-15 18:01:06 +0700

    forgot to divide impulse by mass

--- src/gravity.rs
+++ src/gravity.rs
@@ -14,9 +14,11 @@ pub mod gravity {
     pub fn gravity(time: Res<Time>, mut massive_query: Query<(&Mass, &mut Speed, &Transform)>) {
         let mut vec: Vec<(&Mass, Mut<Speed>, &Transform)> = massive_query.iter_mut().collect();
         for i in 0..vec.len() {
-            //first i elements are already processed, so they will be clipped
+            //first i-1 elements are already processed, so they will be clipped and element i taken
             let (v1, v2) = vec.split_at_mut(i + 1);
+
             let (mass1, speed1, pos1) = &mut v1[i];
+
             for (mass2, speed2, pos2) in v2 {
                 let axis = (pos2.translation - pos1.translation).normalize(); //norm vector from pos1 to pos2 for calculation of vector of impulse
 
@@ -27,10 +29,10 @@ pub mod gravity {
                 ) * time.delta_seconds()
                     * axis;
 
-                speed1.0 += impulse_vec;
-                speed2.0 -= impulse_vec;
+                speed1.0 += impulse_vec / mass1.0;
                 // negate vector of impulse because is points FROM pos1
                 // and we need it pointing TO pos1
+                speed2.0 += (-impulse_vec) / mass2.0;
             }
         }
     }
